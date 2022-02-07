2de6fbe25ebaf45575ac24ef397aacc27b0e5368
אורי ברכה <uriluli@gmail.com>
2022-02-07 16:49:21 +0200

    first commit

---
+++ gui.py
@@ -0,0 +1,114 @@
+from PyQt5.QtCore import QThreadPool, QRunnable, pyqtSlot, QThread, QObject, pyqtSignal, QMutex, QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PIL import Image
+import sys
+from pathlib import Path
+from tensorflow_functions import predict
+import time
+
+
+class Worker(QThread):
+    image_path = pyqtSignal(str)
+    def __init__(self,parent=None):
+
+        self._mutex = QMutex()
+
+        super().__init__()
+
+    def run(self):
+        self._mutex.lock()
+        img = Image.open(file_save_path)
+
+
+        img_path_for_pred = Path("saves/" + time.strftime("%Y%m%d-%H%M%S") + ".jpg")
+        img.save(str(img_path_for_pred))
+
+        img_path_for_show = predict(str(img_path_for_pred))
+        self.image_path.emit(img_path_for_show)
+
+        self.finished.emit()
+        self._mutex.unlock()
+
+
+class Gui:
+    def __init__(self):
+        global  file_save_path
+        Is_Image=None
+        file_save_path=None
+        self.app = QApplication(sys.argv)
+        self.win = QMainWindow()
+        self.win.setGeometry(200, 200, 1200, 1200)
+        width, height = self.win.frameGeometry().width(), self.win.frameGeometry().height()
+        self.win.setWindowTitle("School project Uri Bracha-Gui app")
+        self.worker=Worker()
+
+        self.title = QLabel(self.win)
+        self.title.setFont(QFont("Times", 14, QFont.Bold))
+        self.title.setText("School project Uri Bracha-Gui app")
+        self.title.adjustSize()
+        self.title.move((width // 4) + 50, 0)
+
+        self.text = QLabel(self.win)
+        self.text.setFont(QFont("Times", 14))
+        self.text.setText("choose an image to process:")
+        self.text.move(50, 75)
+        self.text.adjustSize()
+
+        self.button = QPushButton(self.win)
+        self.button.setText("Browse..")
+        self.button.setFont(QFont("Times", 14))
+        self.button.adjustSize()
+        self.button.move(350, 75)
+        self.button.clicked.connect(self.file_dialog)
+
+        self.file_text = QLabel(" file not chosen ...", self.win)
+        self.file_text.setFont(QFont("Times", 14))
+        self.file_text.adjustSize()
+        self.file_text.move(500, 75)
+
+        self.submit = QPushButton(self.win)
+        self.submit.setText("process")
+        self.submit.setFont(QFont("Times", 14))
+        self.submit.adjustSize()
+        self.submit.clicked.connect(self.submit_func)
+        self.submit.move(350, 175)
+
+
+        self.image_show = QLabel(self.win)
+        self.image_show.move(350, 175)
+
+        self.image_result=QLabel(self.win)
+
+        self.image_result = QLabel(self.win)
+
+        self.win.show()
+        sys.exit(self.app.exec_())
+
+    def file_dialog(self):
+        global file_save_path
+        file, check = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "")
+
+        path = Path(file)
+        file_save_path = file
+        self.file_text.setText(path.name)
+        pix=QPixmap(file)
+
+        self.image_show.setPixmap(pix)
+        self.image_show.adjustSize()
+        self.submit.move(350,200+pix.height())
+
+    def submit_func(self):
+            # Step 2: Create a QThread object
+            self.worker.image_path.connect(self.edit_image)
+            self.worker.start()
+
+    def edit_image(self,t):
+        pix=QPixmap(t)
+        self.image_result.setPixmap(pix)
+        self.image_result.adjustSize()
+        self.image_result.move(350,self.button.height()+self.button.y()+pix.height()+150)
+
+if __name__ == '__main__':
+    g = Gui()
---
+++ tensorflow_functions.py
@@ -0,0 +1,85 @@
+import colorsys
+import random
+
+import tensorflow as tf
+import cv2
+import numpy as np
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageColor
+from tensorflow.python.saved_model import tag_constants
+import sys
+import os
+
+
+def draw_bbox(image, bboxes, classes=["glass", "metal", "plastic", "paper", "cardboard", "biodegradable"],
+              show_label=True):
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    for i in range(num_boxes[0]):
+        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
+        coor = out_boxes[0][i]
+        coor[0] = int(coor[0] * image_h)
+        coor[2] = int(coor[2] * image_h)
+        coor[1] = int(coor[1] * image_w)
+        coor[3] = int(coor[3] * image_w)
+
+        fontScale = 0.5
+        score = out_scores[0][i]
+        class_ind = int(out_classes[0][i])
+        bbox_color = colors[class_ind]
+        bbox_thick = int(0.6 * (image_h + image_w) / 600)
+        c1, c2 = (int(coor[1]), int(coor[0])), (int(coor[3]), int(coor[2]))
+        cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
+
+        if show_label:
+            bbox_mess = '%s: %.2f' % (classes[class_ind], score)
+            t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
+            c3 = (c1[0] + t_size[0], c1[1] - t_size[1] - 3)
+            cv2.rectangle(image, c1, (int(np.float32(c3[0])), int(np.float32(c3[1]))), bbox_color, -1)  # filled
+
+            cv2.putText(image, bbox_mess, (c1[0], int(np.float32(c1[1] - 2))), cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+    return image
+
+
+def predict(image_path):
+    model = tf.saved_model.load("model/yolov4-tiny-model", tags=[tag_constants.SERVING])
+    orig_img = cv2.imread(image_path)
+    orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
+    orig_width, orig_height,_ = orig_img.shape
+    orig_img = cv2.resize(orig_img, (256, 256))
+    img = np.array(orig_img).astype(np.float32)
+    infer = model.signatures['serving_default']
+    img = img / 255
+    img = np.expand_dims(img, 0)
+    img = tf.constant(img)
+    pred_bbox = infer(img)
+    for key, value in pred_bbox.items():
+        boxes = value[:, :, 0:4]
+        pred_conf = value[:, :, 4:]
+
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=0.2,
+        score_threshold=0.5
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    new_img = draw_bbox(orig_img, pred_bbox, show_label=True)
+    new_img = Image.fromarray(new_img)
+    new_img=new_img.resize((orig_height,orig_width))
+    save_name = "saves/" + str(Path(image_path).stem) + "_output.jpg"
+    new_img.save(save_name)
+    return save_name
